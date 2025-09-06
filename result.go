16ca3e06918d2a7d6ddf11ae6b2915cd53987fb5
Mirror Ru <mr.mirror.ru@gmail.com>
2025-09-06 23:19:58 +0500

    add SaveVal

--- result.go
+++ result.go
@@ -1,5 +1,10 @@
 package dot
 
+import (
+	"fmt"
+	"reflect"
+)
+
 type Result[T any] struct {
 	val T
 	err error
@@ -7,6 +12,36 @@ type Result[T any] struct {
 
 func MakeResult[T any](val T, err error) Result[T] {
 	return Result[T]{val: val, err: err}
+}
+
+func (r Result[T]) SaveVal(dest any) Result[T] {
+	if r.err != nil {
+		return r
+	}
+
+	// Используем reflection для проверки и присвоения
+	v := reflect.ValueOf(dest)
+	if v.Kind() != reflect.Ptr {
+		panic(fmt.Errorf("expected pointer to value or interface, got %v", v.Kind()))
+	}
+
+	// Получаем значение, на которое указывает pointer
+	v = v.Elem()
+	switch v.Kind() {
+	case reflect.Interface:
+		valueVal := reflect.ValueOf(r.val)
+		if !valueVal.Type().Implements(v.Type()) {
+			panic(fmt.Errorf("value of type %v does not implement interface %v",
+				valueVal.Type(), v.Type()))
+		}
+
+		v.Set(valueVal)
+	default:
+		valueVal := reflect.ValueOf(r.val)
+		v.Set(valueVal)
+	}
+
+	return r
 }
 
 func (r Result[T]) IsErr() bool {
@@ -49,7 +84,7 @@ func (r Result[T]) ToOption() Option[T] {
 	return Option[T]{Val: r.val, Ok: true}
 }
 
-func ConventResult[T1, T2 any](res Result[T1], converter func(src T1) (T2, error)) Result[T2] {
+func ConvertResult[T1, T2 any](res Result[T1], converter func(src T1) (T2, error)) Result[T2] {
 	if res.err != nil {
 		return Result[T2]{err: res.err}
 	}
