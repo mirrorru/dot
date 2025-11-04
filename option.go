9c444f00407e60eb5ab9dcfed77b994ed9a0cbec
Mirror Ru <mr.mirror.ru@gmail.com>
2025-11-05 01:22:22 +0500

    ToOptionExt

--- option.go
+++ option.go
@@ -8,3 +8,15 @@ type Option[T any] struct {
 func ToOption[T any](input T) Option[T] {
 	return Option[T]{Val: input, Ok: true}
 }
+
+func ToOptionEmpty[T any](input T) Option[T] {
+	return Option[T]{}
+}
+
+func ToOptionPtr[T any, PT *T](input PT) Option[T] {
+	if input == nil {
+		return ToOptionEmpty(*input)
+	}
+	return ToOption(*input)
+
+}
